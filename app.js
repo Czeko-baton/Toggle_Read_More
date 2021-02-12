d0f6f809a7b0fc2ed963cd7c303576e38ce411d8
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-02-12 17:44:18 +0100

    Added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+const readMoreBtn = document.getElementById('readMoreBtn');
+const readMoreDiv = document.getElementById('readMoreToggle');
+
+readMoreBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  readMoreDiv.classList.toggle('toggle-active');
+});
