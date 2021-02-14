7f5618feb84b3fcb638ae21abc3b754766fba46a
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-02-14 19:14:14 +0100

    trying make smooth slide, does not working yet

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const readMoreBtn = document.getElementById('readMoreBtn');
 const readMoreDiv = document.getElementById('readMoreToggle');
 
 readMoreBtn.addEventListener('click', (e) => {
-  e.preventDefault();
+  e.preventDefault;
+  // const targer = e.target;
+
   readMoreDiv.classList.toggle('toggle-active');
 });
+
+// e.preventDefault();
+// const current = e.target;
+// const isReadMoreBtn = current.className.includes('readMoreBtn');
+
+// if (!isReadMoreBtn) return;
+
+// const currentText = e.target.parentNode.querySelector('.readMoreDiv');
+// currentText.classList.toggle('toggle-active');
