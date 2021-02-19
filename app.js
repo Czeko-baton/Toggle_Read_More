9ed00d946401f5a486e3ca3afbb0f227649d66c2
Czeko-baton <67105044+Czeko-baton@users.noreply.github.com>
2021-02-19 18:50:00 +0100

    app.js updated

--- app.js
+++ app.js
@@ -8,3 +8,23 @@ readMoreBtn.addEventListener('click', (e) => {
 
   readMoreDiv.classList.toggle('toggle-active');
 });
+
+// ---------------------------------------------------------
+
+const btn = document.querySelector('.read_more_btn');
+const text = document.querySelector('.infoReadMore');
+const infoContainer = document.querySelector('.infoContainer');
+
+infoContainer.addEventListener('click', (e) => {
+  const current = e.target;
+
+  const isReadMoreBtn = current.className.includes('read_more_btn');
+  if (!isReadMoreBtn) return;
+  const currentText = e.target.parentNode.querySelector('.infoReadMore');
+
+  currentText.classList.toggle('infoReadMore--open');
+
+  current.textContent = current.textContent.includes('Read More...')
+    ? 'Read Less...'
+    : 'Read More...';
+});
